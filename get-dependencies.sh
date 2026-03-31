4784cab02334827692bd68306c02c79597b55244
Link4Electronics <link4electronics@gmail.com>
2026-03-31 01:42:10 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,21 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    libsidplayfp   \
+    lxqt-qtplugin  \
+    kvantum        \
+    pipewire-audio \
+    pipewire-jack  \
+    qmmp           \
+    qt6-tools      \
+    qt6ct
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+#make-aur-package qmmp-plugin-pack
 
 # If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
