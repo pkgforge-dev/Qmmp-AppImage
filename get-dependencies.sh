cd050e2bf6993019c157dbe21316cf299a86206a
Link4Electronics <link4electronics@gmail.com>
2026-06-01 16:19:15 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,15 +7,21 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    libsidplayfp   \
-    lxqt-qtplugin  \
-    kvantum        \
-    pipewire-audio \
-    pipewire-jack  \
-    projectm       \
-    qmmp           \
-    qt6-tools      \
-    qt6ct
+    kvantum         \
+    libfbclient     \
+    libmpcdec       \
+    libsidplayfp    \
+    lxqt-qtplugin   \
+    mariadb-libs    \
+    pipewire-audio  \
+    pipewire-jack   \
+    postgresql-libs \
+    projectm        \
+    qmmp            \
+    qt6-tools       \
+    qt6ct           \
+    unixodbc        \
+    wildmidi
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -23,5 +29,3 @@ get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
 #make-aur-package qmmp-plugin-pack
-
-# If the application needs to be manually built that has to be done down here
